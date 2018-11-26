d5bafdcf0bdb4164be89d8783f4fd53bdc012c9e
Lauren Small <lauren.small@detroitlabs.com>
2018-11-26 09:30:36 -0500

    Created XCode Project

---
+++ StarWarsPeople/StarWarsPeople/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsPeople
+//
+//  Created by Lauren Small on 11/26/18.
+//  Copyright © 2018 Lauren Small. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
